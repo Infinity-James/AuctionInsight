d4ef0de67ce814f940c56ea09cf34ba86c8e1b6d
James Valaitis <james@infinityjames.com>
2017-01-26 18:43:47 +0000

    Initial Commit

---
+++ AuctionInsight/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AuctionInsight
+//
+//  Created by James Valaitis on 26/01/2017.
+//  Copyright © 2017 &Beyond. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
