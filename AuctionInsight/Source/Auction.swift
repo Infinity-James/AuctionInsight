7736155d7adac87bcb45a00f1e4d8552f4501e1d
James Valaitis <james@infinityjames.com>
2017-01-27 11:34:08 +0000

    Created Auction model.

---
+++ AuctionInsight/Source/Auction.swift
@@ -0,0 +1,20 @@
+//
+//  Auction.swift
+//  AuctionInsight
+//
+//  Created by James Valaitis on 27/01/2017.
+//  Copyright © 2017 &Beyond. All rights reserved.
+//
+
+import Foundation
+
+//  MARK: Auction
+struct Auction {
+	let id: Int
+	let title: String
+	let rate: Float
+	let centsTotal: Int
+	let term: Int
+	let riskBand: String
+	let closeTime: Date
+}
