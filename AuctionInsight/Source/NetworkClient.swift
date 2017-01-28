2b51ea98a68a8902ceaf9b2cff30cf346d7c0da3
James Valaitis <james@infinityjames.com>
2017-01-28 17:31:55 +0000

    Created protocol for defining a network client.

---
+++ AuctionInsight/Source/NetworkClient.swift
@@ -0,0 +1,11 @@
+//
+//  NetworkClient.swift
+//  AuctionInsight
+//
+//  Created by James Valaitis on 28/01/2017.
+//  Copyright © 2017 &Beyond. All rights reserved.
+//
+
+//  MARK: Network Client
+protocol NetworkClient {
+}
