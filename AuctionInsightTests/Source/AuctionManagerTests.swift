ea95303979d08e58e5262ecb34d4ae9a96e59dec
James Valaitis <james@infinityjames.com>
2017-01-28 16:38:32 +0000

    Created test class against AuctionManager (yet to be created).

---
+++ AuctionInsightTests/Source/AuctionManagerTests.swift
@@ -0,0 +1,16 @@
+//
+//  AuctionManagerTests.swift
+//  AuctionInsight
+//
+//  Created by James Valaitis on 28/01/2017.
+//  Copyright © 2017 &Beyond. All rights reserved.
+//
+
+@testable import AuctionInsight
+import XCTest
+
+//  MARK: Auction Manager Tests
+final class AuctionManagerTests: XCTestCase {
+	//  MARK: Properties
+	var sut = AuctionManager()
+}
