5c3057641362dc4ceade768785ad8f30458c34a8
James Valaitis <james@infinityjames.com>
2017-01-29 12:43:20 +0000

    Created single expectation of NetworkClient.

--- AuctionInsight/Source/NetworkClient.swift
+++ AuctionInsight/Source/NetworkClient.swift
@@ -8,4 +8,11 @@
 
 //  MARK: Network Client
 protocol NetworkClient {
+	/**
+	Loads the provided resource.
+	
+	- Parameter resource:	The resource to be loaded.
+	- Parameter completion:	Called when the resource has finished loading, successfully or not.
+	 */
+	func load<A>(_ resource: Resource<A>, completion: @escaping (Result<A>) -> ())
 }
