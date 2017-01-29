7aab6697f2baabbc244d26ab446510de2ad3655a
James Valaitis <james@infinityjames.com>
2017-01-29 12:48:29 +0000

    Completed concrete implementation of NetworkClient.

--- AuctionInsight/Source/APIClient.swift
+++ AuctionInsight/Source/APIClient.swift
@@ -6,7 +6,28 @@
 //  Copyright © 2017 &Beyond. All rights reserved.
 //
 
+import Foundation
+
 //  MARK: API Client
 final class APIClient: NetworkClient {
-	
+	//  MARK: Properties
+	let session: URLSession
+	//  MARK: Initialization
+	init(session: URLSession = URLSession(configuration: .default)) {
+		self.session = session
+	}
+	//  MARK: NetworkClient
+	func load<A>(_ resource: Resource<A>, completion: @escaping (Result<A>) -> () = assertError) {
+		session.dataTask(with: resource.url, completionHandler: { data, response, _ in
+			let result: Result<A>
+			if let httpResponse = response as? HTTPURLResponse , httpResponse.statusCode == 401 {
+				result = Result.error(NetworkClientError.notAuthenticated)
+			} else {
+				let parsed = data.flatMap(resource.parse)
+				result = Result(parsed, or: NetworkClientError.other)
+			}
+			DispatchQueue.main.async { completion(result) }
+		}) .resume()
+	}
+
 }
