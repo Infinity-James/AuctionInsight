4060a9f36a5f1df4af652aae4111a97fd464ec67
James Valaitis <james@infinityjames.com>
2017-01-29 11:39:19 +0000

    Stubbed out a function for fetching auctions.

--- AuctionInsight/Source/AuctionService.swift
+++ AuctionInsight/Source/AuctionService.swift
@@ -8,10 +8,29 @@
 
 //  MARK: Auction Service
 final class AuctionService {
+	//  MARK: Types
+	/**
+	A block called when an AuctionService operation completes.
+	
+	- Parameter auctions:	The fetched auctions if successful.
+	- Parameter error:		An error which occurred if any.
+	 */
+	typealias AuctionServiceCompleted = ([Auction]?, Error?) -> ()
 	//  MARK: Properties
 	let client: NetworkClient
 	//  MARK: Initialization
 	init(client: NetworkClient) {
 		self.client = client
+	}
+}
+//  MARK: Auctions
+extension AuctionService {
+	/**
+	Executes a fetch request for the available auctions.
+	
+	- Parameter completion:	Called when the operation has completed, successfully or not.
+	 */
+	func auctions(completion: AuctionServiceCompleted) {
+		completion(nil, nil)
 	}
 }
