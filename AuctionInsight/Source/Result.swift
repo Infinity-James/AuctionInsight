e0e971da9b1838b0dc4923422880b409233370c5
James Valaitis <james@infinityjames.com>
2017-01-29 12:39:37 +0000

    Created result type to encapsulate success and failure case.

---
+++ AuctionInsight/Source/Result.swift
@@ -0,0 +1,44 @@
+//
+//  Result.swift
+//  AuctionInsight
+//
+//  Created by James Valaitis on 29/01/2017.
+//  Copyright © 2017 &Beyond. All rights reserved.
+//
+
+//  MARK: Result
+enum Result<A> {
+	///	Operation succeeded.
+	case success(A)
+	///	Operation failed.
+	case error(Error)
+}
+//  MARK: Initialization
+extension Result {
+	public init(_ value: A?, or error: Error) {
+		if let value = value {
+			self = .success(value)
+		} else {
+			self = .error(error)
+		}
+	}
+	///	The result from the operation if it was successful.
+	public var value: A? {
+		guard case .success(let v) = self else { return nil }
+		return v
+	}
+}
+//  MARK: Network Client Error
+public enum NetworkClientError: Error {
+	///	The requested required authentication.
+	case notAuthenticated
+	///	Miscellaneous error.
+	case other
+}
+/**
+Convenience function for logging the error of a result if it failed.
+*/
+func assertError<A>(_ result: Result<A>) {
+	guard case let .error(e) = result else { return }
+	assertionFailure("\(e)")
+}
