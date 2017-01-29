71241169ba04c5f4b8b36a867da8f2339419d058
James Valaitis <james@infinityjames.com>
2017-01-29 16:07:52 +0000

    Set up MVP for AuctionDetail.

---
+++ AuctionInsight/Source/AuctionDetailPresenter.swift
@@ -0,0 +1,12 @@
+//
+//  AuctionDetailPresenter.swift
+//  AuctionInsight
+//
+//  Created by James Valaitis on 29/01/2017.
+//  Copyright © 2017 &Beyond. All rights reserved.
+//
+
+//  MARK: Auction Detail Presenter
+final class AuctionDetailPresenter {
+	
+}
---
+++ AuctionInsight/Source/AuctionDetailViewController.swift
@@ -0,0 +1,14 @@
+//
+//  AuctionDetailViewController.swift
+//  AuctionInsight
+//
+//  Created by James Valaitis on 29/01/2017.
+//  Copyright © 2017 &Beyond. All rights reserved.
+//
+
+import UIKit
+
+//  MARK: Auction Detail View Controller
+final class AuctionDetailViewController: UIViewController {
+	
+}
