9db01ace8955cb4f6d723fcef840c10ee1d332df
James Valaitis <james@infinityjames.com>
2017-01-29 12:29:45 +0000

    Created empty concrete implementation of NetworkClient.

---
+++ AuctionInsight/Source/APIClient.swift
@@ -0,0 +1,12 @@
+//
+//  APIClient.swift
+//  AuctionInsight
+//
+//  Created by James Valaitis on 29/01/2017.
+//  Copyright © 2017 &Beyond. All rights reserved.
+//
+
+//  MARK: API Client
+final class APIClient: NetworkClient {
+	
+}
